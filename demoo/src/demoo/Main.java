5ab8e2ce3a2898566205a874d99e7b609e93d57f
Shreya Shukla <babbishukla099@gmail.com>
2025-06-04 14:34:42 +0530

    main2

--- demoo/src/demoo/Main.java
+++ demoo/src/demoo/Main.java
@@ -3,5 +3,6 @@ package demoo;
 public class Main {
 public static void main(String[] args) {
 	System.out.println("Mera naame hai Giannnnnnn");
+	System.out.println("Mera gala hai bahut surilaa");
 }
 }
